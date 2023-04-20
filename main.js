36b4e36dc4c16d50ed8e85a746778d62525a4311
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-04-20 11:54:28 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Welcome');
+
+const firstName ='Norbert';
+const surName = 'Skwierczynski';
+const age = 37;
+
+console.log(firstName);
+console.log(age);
+
+console.log(`Hey my name is ${firstName} ${surName} and I am ${age} old.`);
