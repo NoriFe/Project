cc715ef97e5bfd160c66ae3938b20534cf07550a
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-05-02 12:14:44 +0100

    added normalization to css styles

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-greet
+
 
 const firstName = "Norbert";
 const surName = "Skwierczynski";
@@ -8,3 +8,20 @@ const age = 37;
 
 console.log(`Hey my name is ${firstName} ${surName} and I am ${age} old.`);
 console.log(`I am so excite as is my first WebPage ;)`)
+
+const deathStar= {
+    diameter:  120000,
+    fire: (target) =>{
+        console.log(`${target} destroyed`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Deadth Star',
+    population: 10000,
+    commander:{
+        name: 'Darth Vader',
+        age: 40,
+        
+    }
+}
+console.log(deathStar);
